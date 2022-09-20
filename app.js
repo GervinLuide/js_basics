db5170d935d15bcf55679bcc2a57b8ecbfce12d3
Gervin Luide <gervin.luide@khk.ee>
2022-09-20 08:17:59 +0300

    Create project files

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-console.log("hello world")
+
+// let
+let name="Gervin Luide"
+
+// const
+const surname ="Luide"
+
+// arrays
+const numbers = [1, 2, 3, 4, 5]
+console.log(numbers)
+numbers.push(6)
+console.log(numbers)
+
+//objects
+
+const person ={
+    firstname: "Gervin",
+    surname: "Luide"
+}
+
+console.log(person)
+console.log(person.firstname)
+console.log(person.surname)
+person.department = "ikt"
+console.log(person)
