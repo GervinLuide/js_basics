350e9939fa947fa7ddbbb42cdeea17053fc0a5a4
Gervin Luide <gervin.luide@khk.ee>
2022-09-21 06:53:01 +0300

    Arrays

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
-const firstname = "gervin"
-const surname = "luide"
-const email = "gervin.luide@voco.ee"
-const department = "IKT"
+const numbers1 = [43, 56, 33, 23, 44, 36, 5]
+const numbers2 = new Array(22, 45, 33, 76, 54)
 
-let result = "<ul>" +
-    "<li> <b>Name</b>" + firstname + "</b>" +
-    "<li>" + surname + "</li>" +
-    "<li>" + email + "</li>" +
-    "<li>" + department + "</li>" +
-    "</ul>"
+let val
 
-result = `<ul>
-<li> <b>name:</b>${firstname} </li>
-<li> <b>surname:</b>${surname} </li>
-<li> <b>email:</b>${email} </li>
-<li> <b>department:</b>${department} </li>
-</ul>`
-console.log(result)
+val = numbers1.length
+vall = numbers2.length
+
+val = numbers1[3]
+val = numbers1.indexOf(36)
+val = Array.isArray(numbers2)
+numbers1.push(250)
+numbers1.pop()
+numbers1.unshift(120)
+numbers1.shift()
+val = numbers1.concat(numbers2)
+numbers1.sort(function (x, y) {
+    return y - x
+})
+numbers1.sort(function (x, y) {
+    return x - y
+})
+console.log(numbers1)
