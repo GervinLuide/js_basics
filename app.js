c5f57952a9cd8dd1f3673680708d5c76e540ddf0
Gervin Luide <gervin.luide@khk.ee>
2022-09-22 13:17:33 +0300

    Date proccessing

--- app.js
+++ app.js
@@ -1,46 +1,21 @@
-const person ={
-    firstname: "Kadi",
-    surname: "Tamm",
-    age: 40,
-    email: "Kadi.tamm@gmail.com",
-    hobbies: ["tennis", "korvpall"],
-    address: {
-        city: "voru",
-        country: "vorumaa"
-    },
-    getBirthYear: function (){
-        return 2022 - this.age
-    }
-}
+const today = new Date()
 
 let val
-val = person
-//kaks valikut:
-val = person.firstname
-val = person["surname"]
-val = person["hobbies"][1]
-val = person.hobbies[0]
-val = person.address.city
-val = person.address["city"]
-val = person.getBirthYear()
+
+val = today
+val = today.getMonth() // 0 -januar
+val = today.getDate()
+val = today.getDay()
+val = today.getFullYear()
+val = today.getHours()
+
+val = new Date("09/22/2021")
+
+val = new Date("September 23 2022")
+
+val = new Date()
+val.setDate(23)
+val.setFullYear(2023)
+
+
 console.log(val)
-
-
-/*
-for(let i = 0; i < person.hobbies.length; i++){
-    console.log(person.hobbies[i])
-}
-
-/*person.hobbies.forEach(function (hobbie, index, hobbies)){
-    console.log(hobbie + "on element indeksiga " + index)
-    console.log(hobbies)
-}
-
- */
-
-person.hobbies.forEach((hobbie, index) => {
-    console.log(hobbie + "on element indeksiga " + index)
-}
-
-
-
