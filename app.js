86e38798fafa1b882189faf8e0c6aff05ae4fe30
Gervin Luide <gervin.luide@khk.ee>
2022-10-20 18:59:50 +0300

    DOM selector

--- app.js
+++ app.js
@@ -1,37 +1,11 @@
-val = document.getElementById('task-form')
-val = document.getElementById('task-form').id
-val = document.getElementById('task-form').className
+const list = document.querySelector('ul')
+const listItem = document.querySelector('li')
 
-const taskTitle = document.getElementById('task-title')
+val = list
+val = list.childNodes
+val = list.childNodes[1]
+val = list.childNodes[1].nodeName
+val = list.childNodes[1].nodeType
 
-// style
-taskTitle.style.background = '#333'
-taskTitle.style.color = '#fff'
-taskTitle.style.padding = '15px'
-// taskTitle.style.display = 'none'
-
-// content
-taskTitle.textContent = 'Defined Tasks'
-taskTitle.innerText = 'My Favorite Tasks'
-taskTitle.innerHTML = '<b style="color: red">My Tasks</b>'
-
-
-val = document.querySelector('#task-title')
-val = document.querySelector('.card-title')
-val = document.querySelector('ul')
-
-// const task = document.querySelector('li')
-// task.style.color = 'green'
-
-val = document.querySelector('li:last-child')
-// document.querySelector('li:nth-child(odd)').style.background = '#ccc'
-// document.querySelector('li:nth-child(even)').style.background = '#f4f4f4'
-
-// multiply elements
-
-val = document.querySelectorAll('li')
-oddLi = document.querySelectorAll('li:nth-child(odd)')
-
-oddLi.forEach((li) => {
-    li.style.background = '#ddd'
-})
+val = list.firstChild
+val = list.firstElementChild
